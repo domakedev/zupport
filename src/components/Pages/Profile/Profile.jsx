817a8c6323aff2685eaabc30ca05569b91713c52
Juan Bernal <juanbernalcode@gmail.com>
2021-12-08 01:21:32 -0600

    making page profile update 1 / juan-sprint-3

--- src/components/Pages/Profile/Profile.jsx
+++ src/components/Pages/Profile/Profile.jsx
@@ -8,9 +8,10 @@ import styled from "styled-components";
 
 const ImgProfile = styled.div `
     margin: auto;
-    margin-top: 20px;
-    width: 300px;
-    height: 300px;
+    margin-top: 10px;
+    //border: solid 2px green; 
+    width: 220px;
+    height: 220px;
      
     display: flex;
     justify-content: center;
@@ -24,17 +25,62 @@ const ImgProfile = styled.div `
     }
 `;
 
+const ContNameUser = styled.div `
+    margin: auto;
+    margin-top: 10px;
+    margin-bottom: 15px;
+    background-color: gray;
+    width: 250px;
+    height: 40px;
+    padding: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ContAboutUser = styled.div `
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: solid 2px green;
+    width: 500px;
+
+`;
+
+const Description = styled.div `
+    border: solid 2px black;
+`;
+
+const NameUs = 'Lionel Messi';
+
+const NameUser = () => {
+	return (
+		<h2>{NameUs}</h2>
+	);
+};
+
+const description_user = 'Soy un gato, que se cree loro, Sin emoji';
 
 
-
-
-function Profile() {
+const Profile= () => {
 	return (
 		<>
 		<Header/>
+
 		<ImgProfile>
 		    <img src={imgProfile} alt="Main image Profile" />
 		</ImgProfile>
+
+		<ContNameUser>
+		    <NameUser/>
+		</ContNameUser>
+
+		<ContAboutUser>
+		<h3>Algo sobre mi</h3>
+		<Description>{description_user}</Description>
+		</ContAboutUser>
+
 		<Footer/>
 		</>
 	);
