9b78ab65749ccfa1510f65d861d937c02f934de7
Juan Bernal <juanbernalcode@gmail.com>
2022-01-03 12:34:37 -0500

    in proccess

--- src/components/Pages/App.js
+++ src/components/Pages/App.js
@@ -8,17 +8,19 @@ import "../../css/App.css";
 // import Register from "./Register/Register";
 // import Communities from "./Communities/Communities";
 // import CreateCommunitie from "./CreateCommunitie/CreateCommunitie"
-import HelpPost from "./NewPost/HelpPost"
+//import HelpPost from "./NewPost/HelpPost"
 //import Landing from "./Landing/Landing";
  /*import P404 from "./404/404";
  import { Login } from "./Login/Login";
  import { Register } from "./Register/Register";*/ 
- import {CommunityPosts} from "./CommunityPosts/CommunityPosts"
+ //import {CommunityPosts} from "./CommunityPosts/CommunityPosts"
  import  Profile  from "./Profile/Profile";
-function App() {
+
+
+ function App() {
   return (
     <Profile></Profile>
-  );
+  );  
 }
 
 export default App;
