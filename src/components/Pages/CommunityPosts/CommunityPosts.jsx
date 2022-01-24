de8e529d347f936749be3aed1b95d2eeca442457
Daniel Ochoa <dochoahenao@gmail.com>
2022-01-24 11:19:06 -0500

    Fixed double call

--- src/components/Pages/CommunityPosts/CommunityPosts.jsx
+++ src/components/Pages/CommunityPosts/CommunityPosts.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import WelcomeCommunity from './WelcomeCommity/WelcomeCommunity';
